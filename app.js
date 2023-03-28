bac9d5c4cc0c6303e0f6bdbf48964100d21a0aa6
Burak Tanyel <buraktanyel34@gmail.com>
2023-03-28 17:35:23 +0300

    makale mobil fix

--- app.js
+++ app.js
@@ -1569,3 +1569,14 @@ ball.addEventListener("click", function(){
 })
 /* toggle ball start */
 
+
+
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
